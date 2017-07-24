3b6db03518ed6a257690bfb3c0dd4d1e1bb0bf41
vinicius <kolo-polo@hotmail.com>
2017-07-24 13:42:51 -0300

    Teste

---
+++ XcodeIam_Solution/src/main/java/com/example/giovanni/xcodeiam/ACT_Configuracao.java
@@ -0,0 +1,13 @@
+package com.example.giovanni.xcodeiam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ACT_Configuracao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act__configuracao);
+    }
+}
