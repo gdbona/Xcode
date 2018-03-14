389a2b8f13d134012c8a6ab7bc0c36305658b988
ViniciusMTM <vinicius@mtmsistemas.com.br>
2018-03-14 13:51:27 -0300

    Primeira Classe da Model

---
+++ XcodeIam_Solution/src/main/java/com/example/giovanni/xcodeiam/Model/CadastroParticipante.java
@@ -0,0 +1,154 @@
+package com.example.giovanni.xcodeiam.Model;
+
+/**
+ * Created by vinicius on 14/03/2018.
+ */
+
+public class CadastroParticipante {
+
+    int Cadastro_ID;
+    double CgcCPF;
+    String NmRazaoSocial;
+    String NmFantasia;
+    char FgTipoPessoa;
+    String InscricaoEstadual;
+    String RegistroGeral;
+    int IdCEP;
+    String Numero;
+    String Complemento;
+    String NrTelefone;
+
+    public int getCadastro_ID() {
+        return Cadastro_ID;
+    }
+
+    public void setCadastro_ID(int cadastro_ID) {
+        Cadastro_ID = cadastro_ID;
+    }
+
+    public double getCgcCPF() {
+        return CgcCPF;
+    }
+
+    public void setCgcCPF(double cgcCPF) {
+        CgcCPF = cgcCPF;
+    }
+
+    public String getNmRazaoSocial() {
+        return NmRazaoSocial;
+    }
+
+    public void setNmRazaoSocial(String nmRazaoSocial) {
+        NmRazaoSocial = nmRazaoSocial;
+    }
+
+    public String getNmFantasia() {
+        return NmFantasia;
+    }
+
+    public void setNmFantasia(String nmFantasia) {
+        NmFantasia = nmFantasia;
+    }
+
+    public char getFgTipoPessoa() {
+        return FgTipoPessoa;
+    }
+
+    public void setFgTipoPessoa(char fgTipoPessoa) {
+        FgTipoPessoa = fgTipoPessoa;
+    }
+
+    public String getInscricaoEstadual() {
+        return InscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        InscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getRegistroGeral() {
+        return RegistroGeral;
+    }
+
+    public void setRegistroGeral(String registroGeral) {
+        RegistroGeral = registroGeral;
+    }
+
+    public int getIdCEP() {
+        return IdCEP;
+    }
+
+    public void setIdCEP(int idCEP) {
+        IdCEP = idCEP;
+    }
+
+    public String getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(String numero) {
+        Numero = numero;
+    }
+
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        Complemento = complemento;
+    }
+
+    public String getNrTelefone() {
+        return NrTelefone;
+    }
+
+    public void setNrTelefone(String nrTelefone) {
+        NrTelefone = nrTelefone;
+    }
+
+    public String getNrCelular() {
+        return NrCelular;
+    }
+
+    public void setNrCelular(String nrCelular) {
+        NrCelular = nrCelular;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public char getFgAtivo() {
+        return FgAtivo;
+    }
+
+    public void setFgAtivo(char fgAtivo) {
+        FgAtivo = fgAtivo;
+    }
+
+    public String getSgUser() {
+        return SgUser;
+    }
+
+    public void setSgUser(String sgUser) {
+        SgUser = sgUser;
+    }
+
+    public String getDtManutencao() {
+        return DtManutencao;
+    }
+
+    public void setDtManutencao(String dtManutencao) {
+        DtManutencao = dtManutencao;
+    }
+
+    String NrCelular;
+    String Email;
+    char FgAtivo;
+    String SgUser;
+    String DtManutencao;
+}
